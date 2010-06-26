6a684a1e8c737d7681afbb476f5358fce8bf6646
mkkim <jumper00@naver.com>
2010-06-26 21:04:49 +0900

    test

---
+++ test.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+void main()
+{
+	printf("hello world \n");
+	printf("hello world _\n");
+	printf("create release_1 branch \n");
+
+}
---
+++ test2/test2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+void main()
+{
+	printf("hello world 2 \n");
+	printf("hello world 2 _\n");
+}
